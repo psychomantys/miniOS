1498b5479346d386a07e7f1f473db66fd0d764b5
psychomantys <psycho.mantys@gmail.com>
2012-09-15 04:47:59 -0300

    Cmake funcionando. COnfundi na hora de dar o commit.

--- src/kernel/i386/flags.cmake
+++ src/kernel/i386/flags.cmake
@@ -1,10 +1,7 @@
 SET (ISA_C_FLAGS "-nostdlib -nostdinc -fno-builtin -fno-stack-protector")
-SET (ISA_C_FLAGS "-nostdlib -nostdinc -fno-builtin -fno-stack-protector")
-SET (ISA_EXE_LINKER_FLAGS "-T${CMAKE_CURRENT_LIST_DIR}/src/link.ld")
+SET (ISA_CXX_FLAGS "-nostdlib -nostdinc -fno-builtin -fno-stack-protector")
+#SET (ISA_EXE_LINKER_FLAGS "-T${CMAKE_CURRENT_LIST_DIR}/src/kernel/link.ld")
 
 # Obtain sources for the ISA
-FILE(GLOB ISA_SRCS "*.c" "*.cpp" "*.asm")
-
-#SET(PLATFORM_LINK "${ISA}/link.ld")
-FILE(GLOB ISA_SRCS "${ISA}/*.c" "${ISA}/*.cpp" "${ISA}/*.asm")
+#FILE(GLOB ISA_SRCS "*.c" "*.cpp" "*.asm")
 
--- src/kernel/i386/pc/flags.cmake
+++ src/kernel/i386/pc/flags.cmake
@@ -1,2 +1,3 @@
-FILE(GLOB PLATFORM_SRCS "*.cpp" "*.c" "*.asm")
+#FILE(GLOB PLATFORM_SRCS "*.cpp" "*.c" "*.asm")
+#SET(PLATFORM_LINK "${CMAKE_CURRENT_LIST_DIR}/src/kernel/i386/pc/link.ld")
 
