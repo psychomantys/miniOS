145a606429a70206c13511361c106b8f57cc4522
psychomantys <psycho.mantys@gmail.com>
2012-09-16 19:05:15 -0300

    src/kernel/arch: 	Criei um diretorio para separar os codigos especificos de uma 	arquitetura. 	Movi os codigo do i386 pc para la.
    src/kernel/compiler/gcc:
    	Criei um diretorio para separar os codigos especificos de um compilador.
    	Adiconei o codigo do gcc e as flags dele lá.

---
+++ src/kernel/compiler/gcc/flags.cmake
@@ -0,0 +1,7 @@
+SET (ISA_C_FLAGS "-nostdlib -nostdinc -fno-builtin -fno-stack-protector -Wno-main -nostartfiles")
+SET (ISA_CXX_FLAGS "-nostdlib -nostdinc -fno-builtin -fno-stack-protector -Wno-main -nostartfiles")
+#SET (ISA_EXE_LINKER_FLAGS "-T${CMAKE_CURRENT_LIST_DIR}/src/kernel/link.ld")
+
+# Obtain sources for the ISA
+#FILE(GLOB ISA_SRCS "*.c" "*.cpp" "*.asm")
+
