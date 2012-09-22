a12dabf6e3902a2dbc1f127c0c281cba764b14bc
psychomantys <psycho.mantys@gmail.com>
2012-09-21 23:34:06 -0300

    CMakeLists.txt: 	Adicionado suporte para usar uma biblioteca C com a função 	LOAD_LIBC_PROFILE. 	Usando principalmente a PDCLib como biblioteca C, mas fazendo 	genenrico o suficiente para trocar facil.
    src/libc/CMakeLists.txt:
    	Implementação da função LOAD_LIBC_PROFILE.
    	Inclui os diretorios que precisam ser incluido para libc funcionar
    	e ainda permite trocar que lib esta usando de forma relamtivamente
    	facil.
    
    src/libc/PDClib/flags.cmake:
    	Adicionado arquivo de flags especificas para incluir da PDCLib.
    	Aqui fica a pasta com os cabeçalhos que vai ser passado para fora.

---
+++ src/libc/PDClib/flags.cmake
@@ -0,0 +1,6 @@
+# Set flags of include directory with hearders file for libc
+set(LIBC_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/src/libc/PDClib/build/includes")
+
+# Now export our output variables
+set(LIBC_STATIC "${PROJECT_SOURCE_DIR}/src/libc/PDClib/build/pdclib.a")
+
